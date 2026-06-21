535157a9946e0db49541b9c060c074ad67228aa9
root <root@localhost>
2026-06-21 18:39:55 +0700

    Cloud Build Request - 2026-06-21 18:39:55

--- app/src/main/kotlin/com/v2ray/ang/viewmodel/MainViewModel.kt
+++ app/src/main/kotlin/com/v2ray/ang/viewmodel/MainViewModel.kt
@@ -1,13 +1,13 @@
 package com.v2ray.ang.viewmodel
 
 import android.app.Application
-import android.arch.lifecycle.AndroidViewModel
-import android.arch.lifecycle.MutableLiveData
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import com.v2ray.ang.AngApplication
 import com.v2ray.ang.AppConfig
 import com.v2ray.ang.R
@@ -20,6 +20,7 @@ import com.v2ray.ang.util.V2rayConfigUtil
 import kotlinx.coroutines.*
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
+
     val isRunning by lazy { MutableLiveData<Boolean>() }
     val updateListAction by lazy { MutableLiveData<Int>() }
     val updateTestResultAction by lazy { MutableLiveData<String>() }
@@ -33,7 +34,11 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     }
 
     override fun onCleared() {
-        getApplication<AngApplication>().unregisterReceiver(mMsgReceiver)
+        try {
+            getApplication<AngApplication>().unregisterReceiver(mMsgReceiver)
+        } catch (e: Exception) {
+            // Ignore if receiver not registered
+        }
         Log.i(AppConfig.ANG_PACKAGE, "Main ViewModel is cleared")
         super.onCleared()
     }
@@ -41,23 +46,28 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     fun testAllTcping() {
         tcpingTestScope.coroutineContext[Job]?.cancelChildren()
         Utils.closeAllTcpSockets()
+
         for (k in 0 until AngConfigManager.configs.vmess.count()) {
             AngConfigManager.configs.vmess[k].testResult = ""
             updateListAction.value = -1 // update all
         }
+
         for (k in 0 until AngConfigManager.configs.vmess.count()) {
             var serverAddress = AngConfigManager.configs.vmess[k].address
             var serverPort = AngConfigManager.configs.vmess[k].port
+
             if (AngConfigManager.configs.vmess[k].configType == EConfigType.CUSTOM.value) {
-                val serverOutbound = V2rayConfigUtil.getCustomConfigServerOutbound(getApplication(),
-                        AngConfigManager.configs.vmess[k].guid) ?: continue
+                val serverOutbound = V2rayConfigUtil.getCustomConfigServerOutbound(
+                    getApplication(), AngConfigManager.configs.vmess[k].guid
+                ) ?: continue
                 serverAddress = serverOutbound.getServerAddress() ?: continue
                 serverPort = serverOutbound.getServerPort() ?: continue
             }
+
             tcpingTestScope.launch {
-                AngConfigManager.configs.vmess.getOrNull(k)?.let {  // check null in case array is modified during testing
+                AngConfigManager.configs.vmess.getOrNull(k)?.let {
                     it.testResult = Utils.tcping(serverAddress, serverPort)
-                    launch(Dispatchers.Main) {
+                    withContext(Dispatchers.Main) {
                         updateListAction.value = k
                     }
                 }
@@ -66,10 +76,10 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     }
 
     fun testCurrentServerRealPing() {
-        val socksPort = 10808//Utils.parseInt(defaultDPreference.getPrefString(SettingsActivity.PREF_SOCKS_PORT, "10808"))
         GlobalScope.launch(Dispatchers.IO) {
+            val socksPort = 10808
             val result = Utils.testConnection(getApplication(), socksPort)
-            launch(Dispatchers.Main) {
+            withContext(Dispatchers.Main) {
                 updateTestResultAction.value = result
             }
         }
@@ -78,12 +88,8 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     private val mMsgReceiver = object : BroadcastReceiver() {
         override fun onReceive(ctx: Context?, intent: Intent?) {
             when (intent?.getIntExtra("key", 0)) {
-                AppConfig.MSG_STATE_RUNNING -> {
-                    isRunning.value = true
-                }
-                AppConfig.MSG_STATE_NOT_RUNNING -> {
-                    isRunning.value = false
-                }
+                AppConfig.MSG_STATE_RUNNING -> isRunning.value = true
+                AppConfig.MSG_STATE_NOT_RUNNING -> isRunning.value = false
                 AppConfig.MSG_STATE_START_SUCCESS -> {
                     getApplication<AngApplication>().toast(R.string.toast_services_success)
                     isRunning.value = true
@@ -92,9 +98,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
                     getApplication<AngApplication>().toast(R.string.toast_services_failure)
                     isRunning.value = false
                 }
-                AppConfig.MSG_STATE_STOP_SUCCESS -> {
-                    isRunning.value = false
-                }
+                AppConfig.MSG_STATE_STOP_SUCCESS -> isRunning.value = false
             }
         }
     }
